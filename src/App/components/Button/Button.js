c65cec44ea3bab8ac1ed2e7c9a17bb973556751f
lambert olivier <microdroide@gmail.com>
2021-11-17 14:58:19 +0100

    feat: ajout propTypes

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './Button.css';
+import PropTypes from 'prop-types'
+
 
 /**
  * Composant "Benjamin button"
@@ -11,5 +13,10 @@ const Button = (props) => {
   console.log(props);
   return <button className="Button" onClick={ (evt)=>{props.onButtonClicked()} }>{props.text}</button>;
 };
+
+Button.propTypes={
+  text: PropTypes.string.isRequired,
+  onButtonClicked: PropTypes.func.isRequired,
+}
 
 export default Button;
