1a89aefaf56f63cb37d992cafda87cbe2bfd3745
lambert olivier <microdroide@gmail.com>
2021-11-17 16:27:39 +0100

    setState mais on en voit pas dans les logs

--- src/App/App.js
+++ src/App/App.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 import Button from './components/Button/Button';
 
 class App extends React.Component {
-  counter=0;
+  counter = 0;
 
   constructor(props) {
     super(props);
+    this.state = { counter: 0, value2: 0 };
   }
 
   render() {
-    return <div className="App">
-      counter: {this.counter}<br/>
-      <Button text="Soustraire 1" onButtonClicked={ () => {} } bgColor="tomato"></Button>
-      <Button text="Ajouter 1" onButtonClicked={ () => {} } bgColor="green"></Button>
-    </div>;
+    return (
+      <div className="App">
+        counter: {this.counter}
+        <br />
+        {/* Des commentaires */}
+        <Button
+          text="Soustraire 1"
+          onButtonClicked={() => {
+            this.setState({ counter: this.state.counter -1 });
+            console.log(this.counter);
+          }}
+          bgColor="tomato"
+        ></Button>
+        <Button
+          text="Ajouter 1"
+          onButtonClicked={() => {
+            this.setState({ counter: this.state.counter  +1 });
+            console.log(this.counter);
+          }}
+          bgColor="green"
+        ></Button>
+      </div>
+    );
   }
 }
 
