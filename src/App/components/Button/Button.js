7963003bacd622abd247fefee2938feedb8d5365
lambert olivier <microdroide@gmail.com>
2021-11-17 14:13:08 +0100

    feat: ajout du benjamin button. Non testé.

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Button.css';
+
+/**
+ * Composant "Benjamin button"
+ * 
+ * @param {*} props props du composant
+ * @returns component render
+ */
+const Button = (props) => {
+  return <button className="Button">Benjamin button</button>;
+};
