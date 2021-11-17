e402c6344d3e348a2bdd6184b9966b7666c23359
lambert olivier <microdroide@gmail.com>
2021-11-17 14:21:19 +0100

    fix: le bouton benjamin fonctionne

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Button from './components/Button/Button'
+
 //import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      HELLO WORLD !
+      <p>HELLO WORLD !</p>
+      <Button/>        
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -10,3 +10,5 @@ import './Button.css';
 const Button = (props) => {
   return <button className="Button">Benjamin button</button>;
 };
+
+export default Button;
