7775b7f80f3933d22880f9debf0679c84fd0662c
lambert olivier <microdroide@gmail.com>
2021-11-17 15:07:30 +0100

    feat: ajout de la couleur au bouton pas ici

--- src/App/App.js
+++ src/App/App.js
@@ -10,7 +10,7 @@ function App() {
       <p>HELLO WORLD !</p>
       <Button text="OK" onButtonClicked={ (arg) => { alert('click sur OK')}}/>        
       <Button text="cancel" onButtonClicked={ (arg) => { alert('click sur cancel')}}/>
-      <Button text="User ne cliquer pas ICI" onButtonClicked={ (arg) => { alert('click sur pas ICI')}}/>
+      <Button text="User ne cliquer pas ICI" bgColor="tomato" onButtonClicked={ (arg) => { alert('click sur pas ICI')}}/>
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -11,12 +11,17 @@ import PropTypes from 'prop-types'
  */
 const Button = (props) => {
   console.log(props);
-  return <button className="Button" onClick={ (evt)=>{props.onButtonClicked()} }>{props.text}</button>;
+  return <button 
+  className="Button"
+  style={{backgroundColor:props.bgColor,color:props.color}} 
+  onClick={ (evt)=>{props.onButtonClicked()} }>{props.text}</button>;
 };
 
 Button.propTypes={
   text: PropTypes.string.isRequired,
   onButtonClicked: PropTypes.func.isRequired,
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
 }
 
 export default Button;
