e91869c5cac2510d9c7191f8c944122bd6a68628
lambert olivier <microdroide@gmail.com>
2021-11-17 16:15:54 +0100

    feat: bouton ajouter soustraire

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Button from './components/Button/Button';
 
-class App extends Component {
+class App extends React.Component {
+  counter=0;
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return (
-      <div>
-        <Button text="hello" bgColor="blue" style={{ fontSize: '15px' }}/>
-      </div>
-    );
+    return <div className="App">
+      counter: {this.counter}<br/>
+      <Button text="Soustraire 1" onButtonClicked={ () => {} } bgColor="tomato"></Button>
+      <Button text="Ajouter 1" onButtonClicked={ () => {} } bgColor="green"></Button>
+    </div>;
   }
 }
-
-App.propTypes = {
-
-};
 
 export default App;
