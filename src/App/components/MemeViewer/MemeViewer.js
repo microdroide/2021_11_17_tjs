83a5b818d39dee9385ace378ca870e35b1059eb5
lambert olivier <microdroide@gmail.com>
2021-11-18 11:18:08 +0100

    feat: affichage du texte dans le svg
    Affichage du texte "stop la triche" dans le svg meme.

--- src/App/components/MemeViewer/MemeViewer.js
+++ src/App/components/MemeViewer/MemeViewer.js
@@ -3,9 +3,23 @@ import PropTypes from 'prop-types';
 import styles from './MemeViewer.module.scss';
 
 const MemeViewer = (props) => (
-  <svg className={styles.MemeViewer} data-testid="MemeViewer" 
-  viewBox={`0 0 ${props.image.w} ${props.image.h}`}>
-    <image href={props.image.url} x="0" y="0"/>
+  <svg
+    className={styles.MemeViewer}
+    data-testid="MemeViewer"
+    viewBox={`0 0 ${props.image.w} ${props.image.h}`}
+  >
+    <image href={props.image.url} x="0" y="0" />
+    <text
+      x={props.meme.x}
+      y={props.meme.y}
+      fill={props.meme.color}
+      fontSize={props.meme.fontSize} 
+      textDecoration={props.meme.underline?'underline':'none'} 
+      fontStyle={props.meme.fontStyle}
+      fontWeight={props.meme.fontWeight}
+    >
+      {props.meme.text}
+    </text>
   </svg>
 );
 
