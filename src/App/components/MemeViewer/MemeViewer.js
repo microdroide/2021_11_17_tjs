a3389308fa4a5b5c0140e348028ef1cd27646806
lambert olivier <microdroide@gmail.com>
2021-11-18 11:09:23 +0100

    feat: augmentation taille du svg

--- src/App/components/MemeViewer/MemeViewer.js
+++ src/App/components/MemeViewer/MemeViewer.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import styles from './MemeViewer.module.scss';
 
 const MemeViewer = (props) => (
-  <svg className={styles.MemeViewer} data-testid="MemeViewer">
+  <svg className={styles.MemeViewer} data-testid="MemeViewer" 
+  viewBox={`0 0 ${props.image.w} ${props.image.h}`}>
     <image href={props.image.url} x="0" y="0"/>
   </svg>
 );
