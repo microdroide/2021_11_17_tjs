be816214856979f88483e85874bd0f788103e1ae
lambert olivier <microdroide@gmail.com>
2021-11-18 12:13:38 +0100

    feat: ajout flexlayout

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import Button from './components/Button/Button';
-import MemeViewer from './components/MemeViewer/MemeViewer';
+import React, { Component } from "react";
+import Button from "./components/Button/Button";
+import FlexLayout from "./components/FlexLayout/FlexLayout";
+import MemeViewer from "./components/MemeViewer/MemeViewer";
 
 class App extends React.Component {
   counter = 0;
@@ -13,38 +14,44 @@ class App extends React.Component {
   /**
    * Cycle de vie avec didUpdate
    */
-  componentDidUpdate(){
+  componentDidUpdate() {
     console.log(arguments);
-    console.log('%c%s','color:red','le changement est pret et effectif', this.state.counter);
+    console.log(
+      "%c%s",
+      "color:red",
+      "le changement est pret et effectif",
+      this.state.counter
+    );
   }
 
   render() {
     return (
       <div className="App">
-        counter: {this.counter}
-        <br />
-        <MemeViewer
-          meme={{
-            titre: "nom 1er meme",
-            text: "stop la triche",
-            x: 370,
-            y: 600,
-            fontSize: 27,
-            color: "tomato",
-            fontWeight: "900",
-            underline: true,
-            italic: true,
-            frameX: 0,
-            frameY: 0,
-          }}
-          image={{
-            id: 0,
-            url: "img/meme1.jpg",
-            titre: "meme1",
-            h: 778,
-            w: 736,
-          }}
-        />
+        <FlexLayout>
+          <MemeViewer
+            meme={{
+              titre: "nom 1er meme",
+              text: "stop la triche",
+              x: 370,
+              y: 600,
+              fontSize: 27,
+              color: "tomato",
+              fontWeight: "900",
+              underline: true,
+              italic: true,
+              frameX: 0,
+              frameY: 0,
+            }}
+            image={{
+              id: 0,
+              url: "img/meme1.jpg",
+              titre: "meme1",
+              h: 778,
+              w: 736,
+            }}
+          />
+          Dans le flexlayout <br />
+        </FlexLayout>
       </div>
     );
   }
