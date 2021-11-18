c09dd23498814bb463c9d2b3066d71107e2d601a
lambert olivier <microdroide@gmail.com>
2021-11-18 16:10:29 +0100

    fix: visuel sur la decoration du formulaire

--- src/App/components/MemeForm/MemeForm.js
+++ src/App/components/MemeForm/MemeForm.js
@@ -18,43 +18,75 @@ function MemeForm(props) {
       <form>
         <div>
           <h1>Titre</h1>
-          <input type="text" id="f_titre" placeholder="saisir titre" value={props.meme.titre}/>
+          <input
+            type="text"
+            id="f_titre"
+            placeholder="saisir titre"
+            value={props.meme.titre}
+          />
           <hr />
         </div>
         <div>
           <h2>Image</h2>
           <select value={props.meme.imageId}>
             <option value="-1">Aucune</option>
-            {props.images.map((e,i) => <option value={e.id}> { e.titre } </option>) }
+            {props.images.map((e, i) => (
+              <option value={e.id}> {e.titre} </option>
+            ))}
           </select>
           <hr />
         </div>
         <div>
           <h2>text</h2>
-          <input type="text" value={props.meme.text}/>
+          <input type="text" value={props.meme.text} />
           <div className={styles.half}>
             <div>
               <label htmlFor="f_x">x:</label>
               <br />
-              <input type="number" className={styles.smallInput} value={props.meme.x} />
+              <input
+                type="number"
+                className={styles.smallInput}
+                value={props.meme.x}
+              />
             </div>
             <div>
               <label htmlFor="f_y">y:</label>
               <br />
-              <input type="number" className={styles.smallInput} value={props.meme.y}/>
+              <input
+                type="number"
+                className={styles.smallInput}
+                value={props.meme.y}
+              />
+            </div>
+          </div>
+          <hr />
+        </div>
+        <div>
+          <h2>Decorations</h2>
+
+          <label htmlFor="f_color">Couleur</label>
+          <input type="color" id="f_color" value={props.meme.color} />
+          <div className={styles.half}>
+            <div>
+              <label htmlFor="f_underline">underline:</label>
+              <br />
+              <input
+                id="f_underline"
+                type="checkbox"
+                checked={props.meme.underline}
+              />
+            </div>
+            <div>
+              <label htmlFor="f_italic">italic:</label>
+              <br />
+              <input
+                id="f_italic"
+                type="checkbox"
+                checked={props.meme.italic}
+              />
             </div>
           </div>
         </div>
-        <div>
-          <p>Decorations</p>
-        </div>
-
-        <label htmlFor="f_color">Couleur</label><input type="color" id="f_color" value={props.meme.color} />
-      <div className={styles.half}>
-        <div><label htmlFor="f_underline">underline:</label><br /><input id="f_underline" type="checkbox" checked={props.meme.underline} /></div>
-        <div><label htmlFor="f_italic">italic:</label><br /><input id="f_italic" type="checkbox" checked={props.meme.italic} /></div>
-      </div>
-
 
         <div className={styles.half}>
           <Button type="reset" text="Clear" />
